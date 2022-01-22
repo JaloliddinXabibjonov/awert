da63ea730e3708593620a09071aa7ec5d4479086
JaloliddinXabibjonov <88301971+JaloliddinXabibjonov@users.noreply.github.com>
2022-01-22 15:46:08 +0500

    Update EditorArticleService.java

--- src/main/java/com/example/article/servise/EditorArticleService.java
+++ src/main/java/com/example/article/servise/EditorArticleService.java
@@ -1,17 +1,1 @@
-package com.example.article.servise;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-@Service
-public class EditorArticleService {
-
-    @Autowired
-    EditorArticleRepository editorArticleRepository;
-
-
-
-
-
-
-}
