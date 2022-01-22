b1e70b42a7003e63f938b37dab61f8715d5742b5
Jaloliddin <dvdvd>
2022-01-22 15:25:52 +0500

    gf

---
+++ src/main/java/com/example/article/servise/EditorArticleService.java
@@ -0,0 +1,17 @@
+package com.example.article.servise;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EditorArticleService {
+
+    @Autowired
+    EditorArticleRepository editorArticleRepository;
+
+
+
+
+
+
+}
